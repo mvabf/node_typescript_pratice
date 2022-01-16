c4c5d9b8765d33f77b42997c9ba5464954755462
Marcus Baltazar <mvabf_dev@outlook.com>
2022-01-16 14:19:00 -0300

    seeds and static route for uploads added

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes/routes';
 
 const app = express();
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000.')
